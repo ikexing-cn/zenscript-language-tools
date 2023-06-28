cf9487abe1094c7a6523fa8c48df105be88adba0
ikexing-cn <ikexing@qq.com>
2023-06-28 18:59:24 +0800

    chore: using new ts feature

--- packages/shared/src/object.ts
+++ packages/shared/src/object.ts
@@ -1,10 +1,8 @@
 export function objectOmit<T extends object, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
-  const result = {} as unknown as Omit<T, K>
+  const result = {} as any satisfies Omit<T, K>
   for (const key in obj) {
-    if (!keys.includes(key as unknown as K)) {
-      // @ts-expect-error ikwid
+    if (!keys.includes(key as unknown as K))
       result[key] = obj[key]
-    }
   }
 
   return result
