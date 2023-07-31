4ddedb4e4218e65b50580ee2ae19155fe4f92dff
ikexing-cn <ikexing@qq.com>
2023-07-31 16:53:00 +0800

    refactor: test case

--- packages/parser/test/cst-parser.spec.ts
+++ packages/parser/test/cst-parser.spec.ts
@@ -1,95 +1,50 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { ZSLexer } from '../src/lexer'
 import { ZSCstParser } from '../src/cst-parser'
 
-it('ImportDeclaration', () => {
-  const lexResult = ZSLexer.tokenize('import xxx.xxx.xxx;')
-  ZSCstParser.parse(lexResult.tokens)
+describe('CST Parser', () => {
+  function parser(code: string, debug?: boolean) {
+    const lexResult = ZSLexer.tokenize(code)
+    ZSCstParser.parse(lexResult.tokens)
 
-  expect(ZSCstParser.errors.length).toBe(0)
-})
-
-it('VariableDeclaration', () => {
-  const lexResult = ZSLexer.tokenize('var a as bool = true;')
-  ZSCstParser.parse(lexResult.tokens)
-
-  expect(ZSCstParser.errors.length).toBe(0)
-})
-
-it('FunctionDeclaration', () => {
-  const lexResult = ZSLexer.tokenize('function a() {}')
-  ZSCstParser.parse(lexResult.tokens)
-
-  expect(ZSCstParser.errors.length).toBe(0)
-})
-
-it('DExpandFunctionDeclaration', () => {
-  const lexResult = ZSLexer.tokenize('$expand string $toUpper(a as string) as string {}')
-  ZSCstParser.parse(lexResult.tokens)
-
-  expect(ZSCstParser.errors.length).toBe(0)
-})
-
-it('ClassDeclaration / ConstructorDeclaration', () => {
-  const lexResult = ZSLexer.tokenize('zenClass A {}')
-  ZSCstParser.parse(lexResult.tokens)
-  expect(ZSCstParser.errors.length).toBe(0)
-
-  const lexResult2 = ZSLexer.tokenize(`
-    zenClass A {
-      zenConstructor() {}
+    /* eslint-disable no-console */
+    if (debug) {
+      console.log(lexResult.tokens)
+      console.log(ZSCstParser.errors)
     }
-  `)
-  ZSCstParser.parse(lexResult2.tokens)
-  expect(ZSCstParser.errors.length).toBe(0)
-})
 
-it('LambdaFunctionDeclaration', () => {
-  const lexResult = ZSLexer.tokenize('function(a as string) as string {};')
-  ZSCstParser.parse(lexResult.tokens)
+    return ZSCstParser.errors.length
+  }
 
-  expect(ZSCstParser.errors.length).toBe(0)
-})
+  it('ImportDeclaration', () => expect(parser('import xxx.xxx.xxx;')).toBe(0))
 
-it('MapInitializerExpression', () => {
-  const lexResult = ZSLexer.tokenize('{a: 1};')
-  ZSCstParser.parse(lexResult.tokens)
+  it('VariableDeclaration', () => expect(parser('var a as bool = true;')).toBe(0))
 
-  expect(ZSCstParser.errors.length).toBe(0)
-})
+  it('FunctionDeclaration', () => expect(parser('function a() {}')).toBe(0))
 
-it('ArrayInitializerExpression', () => {
-  const lexResult = ZSLexer.tokenize('[1,3,4,5];')
-  ZSCstParser.parse(lexResult.tokens)
+  it('DExpandFunctionDeclaration', () => {
+    expect(parser('$expand string $toUpper(a as string) as string {}')).toBe(0)
+  })
 
-  expect(ZSCstParser.errors.length).toBe(0)
-})
+  it('ClassDeclaration / ConstructorDeclaration', () => {
+    expect(parser('zenClass A {}')).toBe(0)
+    expect(parser('zenClass A { zenConstructor() {} }')).toBe(0)
+  })
 
-it('BracketHandlerExpression', () => {
-  const lexResult = ZSLexer.tokenize('<minecraft:apple:1>;')
-  ZSCstParser.parse(lexResult.tokens)
+  it('LambdaFunctionDeclaration', () => {
+    expect(parser('function(a as string) as string {};')).toBe(0)
+  })
 
-  expect(ZSCstParser.errors.length).toBe(0)
-})
+  it('MapInitializerExpression', () => expect(parser('{a: 1};')).toBe(0))
 
-it('PostfixExpressionRange', () => {
-  const lexResult = ZSLexer.tokenize(`
-    0 to 10;
-    0 .. 10;
-  `)
-  ZSCstParser.parse(lexResult.tokens)
+  it('ArrayInitializerExpression', () => expect(parser('[1,3,4,5];')).toBe(0))
 
-  expect(ZSCstParser.errors.length).toBe(0)
-})
+  it('BracketHandlerExpression', () => expect(parser('<minecraft:apple:*>;')).toBe(0))
 
-it('ComplexType', () => {
-  const lexResult = ZSLexer.tokenize('val typeCase as int[[list]][int][];')
-  ZSCstParser.parse(lexResult.tokens)
+  it('PostfixExpressionRange', () => {
+    expect(parser('0 to 10;')).toBe(0)
+    expect(parser('0 ..10;')).toBe(0)
+  })
 
-  // const res = ZSCstParser.parse(lexResult.tokens)
-  // // @ts-expect-error error
-  // // eslint-disable-next-line no-console
-  // console.log({ res: res.children.Statement[0].children.VariableDeclaration[0].children.TypeLiteral[0].children })
-
-  expect(ZSCstParser.errors.length).toBe(0)
+  it('ComplexType', () => expect(parser('val typeCase as int[[list]][int][];')).toBe(0))
 })
--- packages/parser/test/visitor.spec.ts
+++ packages/parser/test/visitor.spec.ts
@@ -1,71 +1,78 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { ZSLexer } from '../src/lexer'
 import { ZSCstParser } from '../src/cst-parser'
 import { ZenScriptVisitor } from '../src/visitor/visitor'
+import type { ASTProgram } from '../src/types/zs-ast'
 
-it('Visitor Program without Expression', () => {
-  const lexResult = ZSLexer.tokenize(`
-    import a.b.C;
-    
-    global a as bool = false;
-    static b as string = '';
+describe('AST Parser', () => {
+  function parser(code: string) {
+    const lexResult = ZSLexer.tokenize(code)
+    const cst = ZSCstParser.parse(lexResult.tokens)
+    expect(ZSCstParser.errors.length).toBe(0)
+    const ast = new ZenScriptVisitor().visit(cst) as ASTProgram
 
-    zenClass To {}
+    return ast.body[0]
+  }
 
-    function fun() as string {}
+  it('ImportDeclaration', () => expect(parser('import xxx.xxx.xxx;')).toMatchSnapshot())
 
-    $expand string $createArray() as void {}
+  it('GlobalStaticDeclaration', () => {
+    expect(parser('global a as bool = false;')).toMatchSnapshot()
+    expect(parser('static b as string = \'\';')).toMatchSnapshot()
+  })
 
+  it('ClassDeclaration', () => {
+    expect(parser('zenClass To {}')).toMatchSnapshot()
+    expect(parser(`
     zenClass TheClass {
-      zenConstructor(a as string[int][string][]) {}
-      var b as string;
-      var funTypeCase as [function(string)void[]];'
-      var classTypeCase as a.b.C[];
-      var classTypeCase2 as C[];
-      var listTypeCase as [[string]][];
-      function c(c as string) {}
-    }
-  `)
-  const cst = ZSCstParser.parse(lexResult.tokens)
-  expect(ZSCstParser.errors.length).toBe(0)
-  const res = new ZenScriptVisitor().visit(cst)
+      zenConstructor(a as int) {}
+      val b as string = '';
+      function c(c as bool) {}
+    }`)).toMatchSnapshot()
+  })
 
-  expect(res).toMatchSnapshot()
-})
+  it('FunctionDeclaration', () => {
+    expect(parser('function fun() as string {}')).toMatchSnapshot()
+    expect(parser('$expand string $createArray() as void {}')).toMatchSnapshot()
+  })
 
-it('Visitor Expression', () => {
-  const lexResult = ZSLexer.tokenize(`
-    a ? true : false;
+  it('TypeLiteral', () => {
+    expect(parser('a as string[int][string][]')).toMatchSnapshot()
+    expect(parser('b as [function(string)void[]];')).toMatchSnapshot()
+    expect(parser('c as a.b.C[];')).toMatchSnapshot()
+    expect(parser('c as C[];')).toMatchSnapshot()
+    expect(parser('c as [[string]][];')).toMatchSnapshot()
+  })
 
-    // expression
-    1 + 1;
-    abc;
+  // Expression
 
-    // breaket handler
-    <minecraft:apple:*>;
-    1 + (1 + 1);
+  it('AssignExpression', () => expect(parser('a = 1;')).toMatchSnapshot())
 
-    {a : 1, b : 2};
-    {};
+  it('BinaryExpression', () => {
+    expect(parser('a + 1;')).toMatchSnapshot()
+    expect(parser('a - 1 + 1;')).toMatchSnapshot()
+    expect(parser('a + (1 - 1);')).toMatchSnapshot()
+  })
 
-    [1,3,4,5];
-    [];
+  it('CallExpression', () => expect(parser('a.b.c.d()(2,3)(1);')).toMatchSnapshot())
 
-    function(a,b,c){};
+  it('ConditionalExpression', () => expect(parser('a ? true : false;')).toMatchSnapshot())
 
-    a.b.c.d()(2,3)(1);
+  describe('PostFixExpression', () => {
+    it('PostfixExpressionMemberAccess', () => expect(parser('a.b.c.d;')).toMatchSnapshot())
+    it('PostfixExpressionRange', () => expect(parser('1 .. 10;')).toMatchSnapshot())
+    it('PostfixExpressionArray', () => expect(parser('a[1][2][3];')).toMatchSnapshot())
+    it('PostfixExpressionFunctionCall', () => expect(parser('a(1,2)(3)();')).toMatchSnapshot())
+    it('PostfixExpressionAsType', () => expect(parser('a as string;')).toMatchSnapshot())
+    it('PostfixExpressionInstanceofType', () => expect(parser('a instanceof string;')).toMatchSnapshot())
+  })
 
-    0 to 10;
-    10 .. 20;
-
-    a[1][2][3] = 233;
-
-    a as string;
-    a instanceof int;
-  `)
-  const cst = ZSCstParser.parse(lexResult.tokens)
-  expect(ZSCstParser.errors.length).toBe(0)
-  const res = new ZenScriptVisitor().visit(cst)
-
-  expect(res).toMatchSnapshot()
+  describe('PrimaryExpression', () => {
+    it('literal case', () => expect(parser('1;')).toMatchSnapshot())
+    it('identifier case', () => expect(parser('abc;')).toMatchSnapshot())
+    it('bracket handler expr case', () => expect(parser('<minecraft:apple:*>;')).toMatchSnapshot())
+    it('lambda function expr case', () => expect(parser('function(a,b,c){};')).toMatchSnapshot())
+    it('array init expr case', () => expect(parser('[1,3,4,5];')).toMatchSnapshot())
+    it('map init expr case', () => expect(parser('{a : 1, b : 2};')).toMatchSnapshot())
+  })
 })
