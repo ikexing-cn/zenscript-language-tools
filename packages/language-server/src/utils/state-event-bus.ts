244a55c6c14ec08761a63241619ea86fef9cd300
ikexing-cn <ikexing@qq.com>
2023-08-02 16:59:09 +0800

    chore: typo

--- packages/language-server/src/utils/state-event-bus.ts
+++ packages/language-server/src/utils/state-event-bus.ts
@@ -1,8 +1,8 @@
 type StateEventBusCallback = (...args: any[]) => any
 
 export class StateEventBus {
-  private states: Map<String, [boolean, any[]]> = new Map()
-  private events: Map<String, StateEventBusCallback[]> = new Map()
+  private states: Map<string, [boolean, any[]]> = new Map()
+  private events: Map<string, StateEventBusCallback[]> = new Map()
 
   public on(name: string, callback?: any) {
     if (!this.states.has(name)) {
